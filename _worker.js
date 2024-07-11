fa03254a868dd289c298885d47fc49e949ff7a96
zping6688 <128884129+zping6688@users.noreply.github.com>
2024-07-11 13:55:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,7 +11,7 @@ let userID = 'e481f06c-d7cb-4818-848c-d595e85c943a';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
-const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'cf.pangushen.eu.org'];
+const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org'];
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
